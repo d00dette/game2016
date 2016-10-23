96d439023a56d9bd0558867739124451da6beb03
Vincent Ader <Vincent Ader>
2016-10-22 22:57:57 -0400

    Node class

---
+++ Scripts/Tree/Node.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * Node class, holds stuff :)
+ */
+public class Node
+{
+    private string nodeName;
+    private string text;
+    private ArrayList neighbors;
+    
+    public Node(string nodeName)
+    {
+        this.nodeName = nodeName;
+        this.neighbors = new ArrayList();
+        this.text = "N/a";
+    }
+
+    public string getNodeName()
+    {
+        return this.nodeName;
+    }
+
+    public void addNeighbors(ArrayList neighbors)
+    {
+        foreach (Node i in neighbors)
+        {
+            this.neighbors.Add(i);
+        }
+    }
+
+    // setText function
+
+    public string getText()
+    {
+        return this.text;
+    }
+}
